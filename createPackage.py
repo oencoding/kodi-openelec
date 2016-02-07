56a2bd8ebb8922f894d73b6db4eb8ff5dcbec430
Matan Assli <matanasslizada@gmail.com>
2016-02-07 08:39:31 +0200

    Add prints

--- createPackage.py
+++ createPackage.py
@@ -15,16 +15,14 @@ current_time = time.strftime("%Y%m%d%H%M")
 path = os.path.join(OUTPUT_DIR,current_time,STORAGE)
 os.makedirs(path)
 
-my_file = open(path+ "/test.txt", "w")
-my_file.write("Hello")
-my_file.close()
-
 arc_name = current_time + ".tar"
 arc_path = os.path.join(OUTPUT_DIR, arc_name)
 tar = tarfile.open(arc_path,"w")
 
+print "Creating tar..."
 for folder in FOLDERS:
     my_path = os.path.join(STORAGE,folder)
     tar.add(folder,my_path,True)
 tar.close()
-
+print "Finished."
+print "Tar location: %s" %(arc_path)
