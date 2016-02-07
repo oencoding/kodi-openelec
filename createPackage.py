222b3bf17ee3a29580af038c9b7c36934b70aea8
Matan Assli <matanasslizada@gmail.com>
2016-02-07 08:27:35 +0200

    build kodi archive

--- createPackage.py
+++ createPackage.py
@@ -3,15 +3,28 @@ import os
 import tarfile
 import shutil
 
-shutil.rmtree("OUTPUT")
+OUTPUT_DIR = "OUTPUT"
+STORAGE = "storage"
+#FOLDERS = [".kodi", ".config", ".cache"]
+FOLDERS = [".config", ".cache"]
+
+if os.path.isdir(OUTPUT_DIR):
+    shutil.rmtree(OUTPUT_DIR)
+
 current_time = time.strftime("%Y%m%d%H%M")
-path = "OUTPUT/" + current_time + "/storage"
+path = os.path.join(OUTPUT_DIR,current_time,STORAGE)
 os.makedirs(path)
+
 my_file = open(path+ "/test.txt", "w")
 my_file.write("Hello")
 my_file.close()
-os.chdir("OUTPUT")
-tar = tarfile.open(current_time + ".tar","w")
-tar.add(current_time, True)
+
+arc_name = current_time + ".tar"
+arc_path = os.path.join(OUTPUT_DIR, arc_name)
+tar = tarfile.open(arc_path,"w")
+
+for folder in FOLDERS:
+    my_path = os.path.join(STORAGE,folder)
+    tar.add(folder,my_path,True)
 tar.close()
 
